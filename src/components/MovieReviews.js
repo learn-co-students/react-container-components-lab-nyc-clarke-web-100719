c2b98f5b4007645f5f2655b452db1395f7b9f258
jastaffen <jack.staffen@gmail.com>
2019-12-17 13:18:10 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,26 @@
 // Code MovieReviews Here
+import React from 'react';
+// import MovieReview from './MovieReview';
+
+const MovieReviews = ({reviews}) => {
+
+    const renderArticles = () => {
+        return reviews.map(review => {
+            return (
+                < React.Fragment key={review.display_title} >
+                    <li className="review"><strong>{review.byline}</strong> - {review.headline}</li>
+                </ React.Fragment>
+            )
+        })
+    }
+
+    return(
+        <div className="review-list">
+            <ul>
+                {renderArticles()}
+            </ul> 
+        </div>
+    )
+}
+
+export default MovieReviews;
